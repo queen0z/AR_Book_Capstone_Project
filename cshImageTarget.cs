9c3081f0ec9f9c14a9419a20fd6957be8bbd57d2
young_ji_jang <127760283+queen0z@users.noreply.github.com>
2025-05-20 17:25:39 +0900

    rabbit & zara code
    토끼와자라 코드

--- cshImageTarget.cs
+++ cshImageTarget.cs
@@ -1,33 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using Vuforia;
 
-public class ImageTarget: MonoBehaviour
+public class cshImageTarget : MonoBehaviour
 {
+    public bool Marker2;
+    public bool Card1;
     public bool Marker3;
-    public bool AxCard;
+    public bool Card2;
+    public bool Marker4;
+    public bool Card3;
     public bool Marker6;
-    public bool WindCard;
+    public bool Card4;
     public bool Marker9;
-    public bool FireCard;
-    public GameObject tree1;
-    public GameObject brokenTree;
-    public Transform brokenTree_tf;
-    public Transform house1;
-    public GameObject house1G;
-    public Transform wolf;
-    public GameObject wolfG;
-    public Transform fire_tf;
-    public GameObject fire;
-    public Transform smoke_tf;
-    public GameObject smoke;
-    bool hasFired = false;
-    bool hasTree = false;
+    public bool Card5;
+    public Transform picture;
+    public GameObject medicine;
+    public GameObject liver;
+    bool hasLiver = false;
+    public Transform mal_tf;
     bool isMoving1 = true;
-    bool isMoving2 = true;
-    public GameObject woodBreakParticle;
+    // bool isMoving2 = true;
+    public Transform zara;
+    bool isMoving3 = true;
+    public Transform rabbit;
+    public GameObject rabbitG;
+    bool hasRabbitPic = false;
+    public Transform rabbitPic_tf;
+    public GameObject rabbitPic;
+    bool isMoving4 = true;
+    public Transform zaraRabbit;
+    public GameObject notice2;
     public GameObject notice3;
+    public GameObject notice4;
     public GameObject notice6;
     public GameObject notice9;
+    bool zaraMoving1 = true;
+    bool zaraMoving2 = true;
+    bool zaraMoving3 = true;
+
+
+
+    public void OnMarker2()
+    {
+        Marker2 = true;
+    }
+
+    public void OffMarker2()
+    {
+        Marker2 = false;
+    }
 
     public void OnMarker3()
     {
@@ -39,14 +61,14 @@ public class ImageTarget: MonoBehaviour
         Marker3 = false;
     }
 
-    public void OnAxCard()
+    public void OnMarker4()
     {
-        AxCard = true;
+        Marker4 = true;
     }
 
-    public void OffAxCard()
+    public void OffMarker4()
     {
-        AxCard = false;
+        Marker4 = false;
     }
 
     public void OnMarker6()
@@ -59,16 +81,6 @@ public class ImageTarget: MonoBehaviour
         Marker6 = false;
     }
 
-    public void OnWindCard()
-    {
-        WindCard = true;
-    }
-
-    public void OffWindCard()
-    {
-        WindCard = false;
-    }
-
     public void OnMarker9()
     {
         Marker9 = true;
@@ -79,99 +91,218 @@ public class ImageTarget: MonoBehaviour
         Marker9 = false;
     }
 
-    public void OnFireCard()
+    public void OnCard1()
     {
-        FireCard = true;
+        Card1 = true;
     }
 
-    public void OffFireCard()
+    public void OffCard1()
     {
-        FireCard = false;
+        Card1 = false;
     }
 
-    private void Start()
+    public void OnCard2()
     {
+        Card2 = true;
+    }
+
+    public void OffCard2()
+    {
+        Card2 = false;
+    }
+
+    public void OnCard3()
+    {
+        Card3 = true;
+    }
+
+    public void OffCard3()
+    {
+        Card3 = false;
+    }
+
+    public void OnCard4()
+    {
+        Card4 = true;
+    }
+
+    public void OffCard4()
+    {
+        Card4 = false;
+    }
+
+    public void OnCard5()
+    {
+        Card5 = true;
+    }
+
+    public void OffCard5()
+    {
+        Card5 = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Card1 && Marker2)
+        {
+            Destroy(medicine, 1);
+            Invoke("newLiver", 1f);
+        }
+
+        if (Card2 && Marker3)
+        {
+            pictureMove();
+            Invoke("stopMoving1", 1.0f);
+        }
+
+        if (Card3 && Marker4)
+        {
+            zaraMove1();
+            Invoke("zaraStop1", 1.0f);
+            Invoke("zaraMove2", 1.0f);
+            Invoke("zaraStop2", 2.0f);
+            Invoke("zaraMove3", 2.0f);
+            Invoke("zaraStop3", 3.0f);
         
+            newRabbitPicture();
+        } 
+
+        
+        
+
+        if (Card4 && Marker6)
+        {
+            zaraRabbitMove();
+            Invoke("stopMoving4", 2.5f);
+        }
+
+        if (Card5 && Marker9)
+        {
+            rabbitMove();
+            Invoke("stopMoving3", 1.0f);
+        }
+
     }
 
-    private void Update()
+
+    void newRabbitPicture()
     {
-        if(AxCard && Marker3)  	
+        if (!hasRabbitPic)
         {
-            Destroy(tree1, 1);
-            Destroy(notice3);
-            Invoke("newTree", 1f);
-            
+            Instantiate(rabbitPic, rabbitPic_tf.transform);
+            hasRabbitPic = true;
         }
-
-        if(WindCard && Marker6)  
-        {
-            Destroy(notice6);
-            houseMove();
-            Invoke("StopMoving1", 2f);
-        }
-
-        if(FireCard && Marker9)    
-        {
-            Destroy(notice9);
-            if (!hasFired)
-            {
-                Instantiate(fire, fire_tf.position, fire_tf.rotation);
-                Instantiate(smoke, smoke_tf.position, smoke_tf.rotation);
-                hasFired = true; // 파이어가 생성되었음을 표시합니다.
-            }
-            Invoke("wolfMove", 3f);
-            Invoke("StopMoving2", 4.5f);
-        }
-
-    
     }
 
-    void houseMove()
+    void pictureMove()
     {
         if (isMoving1)
         {
-            house1.position += Vector3.right * 4 * Time.deltaTime;
-        }
-
-    }
-
-    void wolfMove()
-    {
-        if (isMoving2)
-        {
-            // 왼쪽 방향으로 이동합니다.
-            wolf.position += Vector3.left * 4 * Time.deltaTime;
+            picture.position += Vector3.left * 4 * Time.deltaTime;
         }
     }
 
-    void newTree()
-    {
-        if (!hasTree)
-        {
-            Instantiate(woodBreakParticle, brokenTree_tf.localPosition, Quaternion.identity);
-            Instantiate(brokenTree, brokenTree_tf.transform);
-            //brokenTree.localScale = new Vector3(2.0f, 2.0f, 2.0f);
-            hasTree = true;
-            Invoke("DestroyParticle", 1.5f);
-        }
-    }
-
-    void StopMoving1()
+    void stopMoving1()
     {
         isMoving1 = false;
-        Destroy(house1G);
     }
 
-    void StopMoving2()
+    void newLiver()
     {
-        isMoving2 = false;
-        Destroy(wolfG);
+        if (!hasLiver)
+        {
+            Instantiate(liver, mal_tf.transform);
+            hasLiver = true;
+        }
     }
 
-    void DestroyParticle()
+    void zaraMove1()
     {
-        Destroy(woodBreakParticle);
+        if (zaraMoving1)
+        {
+            Vector3 moveDirection = Vector3.right + Vector3.back;
+            zara.position += Vector3.right * 3 * Time.deltaTime;
+            zara.position += Vector3.back * 3.5f * Time.deltaTime;
+            zara.rotation = Quaternion.LookRotation(moveDirection.normalized);
+        }
     }
-    
+
+    void zaraMove2()
+    {
+        if (zaraMoving2)
+        {
+            Vector3 moveDirection = Vector3.right + Vector3.forward;
+            zara.position += Vector3.right * 3 * Time.deltaTime;
+            zara.position += Vector3.forward * 3.5f * Time.deltaTime;
+            zara.rotation = Quaternion.LookRotation(moveDirection.normalized);
+        }
+    }
+
+    void zaraMove3()
+    {
+        if (zaraMoving3)
+        {
+            Vector3 moveDirection = Vector3.right + Vector3.back;
+            zara.position += Vector3.right * 3 * Time.deltaTime;
+            zara.position += Vector3.back * 3.5f * Time.deltaTime;
+            zara.rotation = Quaternion.LookRotation(moveDirection.normalized);
+        }
+    }
+
+    void zaraStop1()
+    {
+        zaraMoving1 = false;
+    }
+
+    void zaraStop2()
+    {
+        zaraMoving2 = false;
+    }
+
+    void zaraStop3()
+    {
+        zaraMoving3 = false;
+    }
+
+
+    void rabbitMove()
+    {
+        if (isMoving3)
+        {
+            Vector3 moveDirection = Vector3.right + Vector3.forward;
+            rabbit.position += Vector3.right * 2 * Time.deltaTime;
+            rabbit.position += Vector3.forward * 4 * Time.deltaTime;
+            rabbit.rotation = Quaternion.LookRotation(moveDirection.normalized);
+        }
+    }
+
+    void stopMoving3()
+    {
+        isMoving3 = false;
+        Destroy(rabbitG);
+    }
+
+    void zaraRabbitMove()
+    {
+        if (isMoving4)
+        {
+            zaraRabbit.position += Vector3.right * 2.3f * Time.deltaTime;
+            //zaraRabbit.position += Vector3.down * 10 * Time.deltaTime;
+            zaraRabbit.position += Vector3.back * 2.2f * Time.deltaTime;
+
+        }
+    }
+
+    void stopMoving4()
+    {
+        isMoving4 = false;
+    }
 }
